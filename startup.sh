6bceaeb35fab1814810245fb148b36db8d5fd7ce
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-06-29 12:39:05 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ Xvfb :1 -screen 0 ${RESOLUTION:1600x900x16} &
 sleep 7
 startlxde &
 x11vnc -forever -nopw -httpdir /usr/share/vnc-java/ -httpport ${X11VNCPORT:5800} -shared &
-cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:${VNCPORT:5900} 
+cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:${PORT:5900} 
